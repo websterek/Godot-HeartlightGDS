1e2fe7e11a863a4dab31c9c9ea5de7d6db4dd909
Ruszard <nekorushi@gmail.com>
2018-12-15 17:11:59 +0100

    Remake menu with final graphics

--- src/main_menu/main_menu.gd
+++ src/main_menu/main_menu.gd
@@ -1,21 +1,23 @@
 extends Container
 
-onready var Player1Button = get_node("Options/Player1")
-onready var Player2Button = get_node("Options/Player2")
-onready var Player3Button = get_node("Options/Player3")
+onready var menu_container = get_node("Container/Panel/Margins")
 
 func _ready():
 	self.rect_size = Vector2(
 			ProjectSettings.get_setting("display/window/size/width"), 
 			ProjectSettings.get_setting("display/window/size/height")
 		)
-	
-	Player1Button.connect("pressed", self, "startGame", ["Player1"])
-	Player2Button.connect("pressed", self, "startGame", ["Player2"])
-	Player3Button.connect("pressed", self, "startGame", ["Player3"])
-	pass
+		
+func activateMenu(menuNodeName):
+	for menu in menu_container.get_children():
+		if menu.get_name() != menuNodeName:
+			menu.visible = false
+		else:
+			menu.visible = true
 
 func startGame(playerId):
 	globals.set_current_player(playerId)
 	get_tree().change_scene("res://src/game_manager/game_manager.tscn")
-	pass
+
+func exitGame():
+	get_tree().quit()
